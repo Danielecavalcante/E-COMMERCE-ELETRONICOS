9632d54287abeb8da7eb57f2fd8033b381a11e1e
unknown <daniele.fscavalcante@gmail.com>
2022-05-29 17:09:10 -0300

    Organizando em DDD

---
+++ E-COMMERCE ELETRÔNICOS/Entities/Notifications/Notities.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Notifications
+{
+    public class Notities
+    {
+       
+        public Notities()
+        {
+            Notitycoes = new List<Notities>();
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifies> Notitycoes;
+
+        public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notitycoes.Add(new Notities
+                {
+                    mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidarPropriedadeInt(int valor, string nomePropriedade)
+        {
+
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notitycoes.Add(new Notities
+                {
+                    mensagem = "Valor deve ser maior que 0",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+
+        }
+
+        public bool ValidarPropriedadeDecimal(decimal valor, string nomePropriedade)
+        {
+
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notitycoes.Add(new notities
+                {
+                    mensagem = "Valor deve ser maior que 0",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+
+        }
+
+
+    }
+}
+}
